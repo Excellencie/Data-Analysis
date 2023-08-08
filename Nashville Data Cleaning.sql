fd90ee7f4a964f4554547b52f0b8cc695f5062fb
Excellencie <90435365+Excellencie@users.noreply.github.com>
2023-08-08 17:17:32 +0100

    Update Nashville Data Cleaning.sql

--- Nashville Data Cleaning.sql
+++ Nashville Data Cleaning.sql
@@ -1,130 +1,145 @@
 -- Standardize the date
-SELECT SaleDate, CONVERT(date,SaleDate) as NSaleDate
+SELECT
+  SaleDate,
+  CONVERT(date, SaleDate) AS NSaleDate
 FROM dbo.NashvilleData
 
-update NashvilleData
-set SaleDate = CONVERT(date,SaleDate) 
+UPDATE NashvilleData
+SET SaleDate = CONVERT(date, SaleDate)
 
 
-alter table nashvilledata
-add SaleDateCon date; 
+ALTER TABLE nashvilledata
+ADD SaleDateCon date;
 
-update NashvilleData
-set SaleDateCon = CONVERT(date,SaleDate)
+UPDATE NashvilleData
+SET SaleDateCon = CONVERT(date, SaleDate)
 
 -- Replace property address where null
 
-select *
-from NashvilleData
-order by ParcelID
+SELECT
+  *
+FROM NashvilleData
+ORDER BY ParcelID
 
-select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
-FROM NashvilleData as a 
-JOIN NashvilleData as b
-	ON a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
-WHERE a.PropertyAddress is null
+SELECT
+  a.ParcelID,
+  a.PropertyAddress,
+  b.ParcelID,
+  b.PropertyAddress,
+  ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM NashvilleData AS a
+JOIN NashvilleData AS b
+  ON a.ParcelID = b.ParcelID
+  AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
 
-update a
-SET a.PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
-FROM NashvilleData as a 
-JOIN NashvilleData as b
-	ON a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
-WHERE a.PropertyAddress is null
+UPDATE a
+SET a.PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM NashvilleData AS a
+JOIN NashvilleData AS b
+  ON a.ParcelID = b.ParcelID
+  AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
 
 -- Breaking the property address column into address, city
 
-select
-SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1) as PropAddress,
-SUBSTRING(PropertyAddress, CHARINDEX(',',PropertyAddress)+1, LEN(PropertyAddress)) as PropCity
-from NashvilleData
+SELECT
+  SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) - 1) AS PropAddress,
+  SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress)) AS PropCity
+FROM NashvilleData
 
-alter table nashvilledata
-add PropAddress nvarchar(255); 
+ALTER TABLE nashvilledata
+ADD PropAddress nvarchar(255);
 
-update NashvilleData
-set PropAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1)
+UPDATE NashvilleData
+SET PropAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) - 1)
 
-alter table nashvilledata
-add PropCity nvarchar(255); 
+ALTER TABLE Nashvilledata
+ADD PropCity nvarchar(255);
 
-update NashvilleData
-set PropCity = SUBSTRING(PropertyAddress, CHARINDEX(',',PropertyAddress)+1, LEN(PropertyAddress))
+UPDATE NashvilleData
+SET PropCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress))
 
 -- -- Breaking the property address column into address, city, state
 
-select
-PARSENAME(Replace(OwnerAddress,',','.'), 3),
-PARSENAME(Replace(OwnerAddress,',','.'), 2),
-PARSENAME(Replace(OwnerAddress,',','.'), 1)
-from NashvilleData
+SELECT
+  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),
+  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2),
+  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+FROM NashvilleData
 
-alter table nashvilledata
-add OwnerAddressSplit nvarchar(255); 
+ALTER TABLE Nashvilledata
+ADD OwnerAddressSplit nvarchar(255);
 
-update NashvilleData
-set OwnerAddressSplit = PARSENAME(Replace(OwnerAddress,',','.'), 3)
+UPDATE NashvilleData
+SET OwnerAddressSplit = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
 
-alter table nashvilledata
-add OwnerCity nvarchar(255); 
+ALTER TABLE Nashvilledata
+ADD OwnerCity nvarchar(255);
 
-update NashvilleData
-set OwnerCity = PARSENAME(Replace(OwnerAddress,',','.'), 2)
+UPDATE NashvilleData
+SET OwnerCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
 
-alter table nashvilledata
-add OwnerState nvarchar(255); 
+ALTER TABLE nashvilledata
+ADD OwnerState nvarchar(255);
 
-update NashvilleData
-set OwnerState = PARSENAME(Replace(OwnerAddress,',','.'), 1)
+UPDATE NashvilleData
+SET OwnerState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
 
 -- Update SoldAsVacant to have Yes and No as only values instead of Y & N
 
-select distinct(SoldAsVacant), count(SoldAsVacant)
-from nashvilledata
-group by soldasvacant
-order by 2
+SELECT DISTINCT
+  (SoldAsVacant),
+  COUNT(SoldAsVacant)
+FROM nashvilledata
+GROUP BY soldasvacant
+ORDER BY 2
 
-select SoldAsVacant,
-case when SoldAsVacant = 'Y' then 'Yes'
-	when SoldAsVacant = 'N' then 'No'
-	else SoldAsVacant
-	end
-from NashvilleData
+SELECT
+  SoldAsVacant,
+  CASE
+    WHEN SoldAsVacant = 'Y' THEN 'Yes'
+    WHEN SoldAsVacant = 'N' THEN 'No'
+    ELSE SoldAsVacant
+  END
+FROM NashvilleData
 
 UPDATE NashvilleData
-SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
-	WHEN SoldAsVacant = 'N' then 'No'
-	ELSE SoldAsVacant
-	END;
+SET SoldAsVacant =
+                  CASE
+                    WHEN SoldAsVacant = 'Y' THEN 'Yes'
+                    WHEN SoldAsVacant = 'N' THEN 'No'
+                    ELSE SoldAsVacant
+                  END;
 
 
 -- Remove duplicates
 
-WITH RowNumCTE AS(
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY
-					UniqueID
-					) row_num
-From NashvilleData)
-DELETE 
-FROM RowNumCTE
+WITH RowNumCTE
+AS (SELECT
+  *,
+  ROW_NUMBER() OVER (
+  PARTITION BY ParcelID,
+  PropertyAddress,
+  SalePrice,
+  SaleDate,
+  LegalReference
+  ORDER BY
+  UniqueID
+  ) row_num
+FROM NashvilleData)
+DELETE FROM RowNumCTE
 WHERE row_num > 1
 
 
 --Delete columns that are not in use
 ALTER TABLE NashvilleData
 DROP COLUMN PropertyAddress
-		,OwnerAddress
-		,SalesDateCon -- deleting it because because it's a duplicate
-		,TaxDistrict
-			
+, OwnerAddress
+, SalesDateCon -- deleting it because it's a duplicate
+, TaxDistrict
 
-SELECT *
+
+SELECT
+  *
 FROM NashvilleData
